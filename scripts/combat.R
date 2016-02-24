10175659dfd674801a688f57a6887d9f6c1792e9
Hege MB <hege.m.bovelstad@uit.no>
2016-02-24 14:40:19 +0100

    First commit. As a test.

---
+++ scripts/combat.R
@@ -0,0 +1,14 @@
+
+## Try combat
+batch = labInfo[,"New_plate"]
+edata = log2(exprs(data))
+mod = model.matrix(~as.factor(Case_ctrl),data=background)
+
+## Correct for batch using ComBat
+combat_edata = ComBat(dat=edata, batch=batch, mod=mod, par.prior=TRUE, prior.plots=FALSE)
+## End(Not run)
+
+log2.exprs <- combat_edata
+
+pp <- prcomp(log2.exprs)
+pcData <- data.frame(pp$x)
